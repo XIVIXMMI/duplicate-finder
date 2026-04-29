872672fff588aec40f6de46427e1205d1c05b78b
XIVIXMMI <lethaikhoinguyen@gmail.com>
2026-04-29 12:32:45 +0700

    feat: remove duplicate image to trash

--- src/main.rs
+++ src/main.rs
@@ -80,15 +80,40 @@ fn main() {
     let duplicates = find_dupliactes(images);
 
     if duplicates.is_empty() {
-        println!("No duplicates found!")
-    } else {
-        println!("\n=== Found {} duplicate groups ===\n", duplicates.len());
-        for (hash, files) in &duplicates {
-            println!("Hash: {} ...", &hash[..8]);
-            for file in files {
-                file.display();
-            }
-            println!();
+        println!("No duplicates found!");
+        return;
+    } 
+
+    println!("\n=== Found {} duplicate groups ===\n", duplicates.len());
+
+    for (hash, files) in &duplicates {
+        println!("Hash: {} ...", &hash[..8]);
+        let(keep, to_delete) = files.split_first().unwrap();
+
+        println!("Keep: {:?}", keep.path.file_name().unwrap());
+        
+        for file in to_delete {
+            println!("Delete: {:?}", file.path.file_name().unwrap());
         }
+
+        // confirm before delete
+        println!("Move to trash? [Y/N]: ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "y" || input.trim() == "Y" {
+            for file in to_delete {
+                match trash::delete(&file.path) {
+                    Ok(_) => println!("Moved to Trash: {:?}",file.path.file_name().unwrap()),
+                    Err(e) => println!("Error: {}", e),
+                }
+            }
+        } else if input.trim() == "N" || input.trim() =="n"  {
+            println!("Skiped.");
+        } else {
+            println!("Please enter 'Y' or 'N' ");
+        }
+        println!();
     }
+    
 }
